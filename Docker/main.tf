c5038f3e94456dc9499c3b2d3856b1c5c0e8884b
memor24linux <myemail@example.com>
2024-10-13 12:54:14 +0330

    new updates

--- Docker/main.tf
+++ Docker/main.tf
@@ -11,18 +11,23 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-# Pulls the image
+#sample nginx image from dockerhub via api
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = "false"
 }
 
-# Create a container
+#creates the nginx container using the created image
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "sth"
+  name  = "var.docker_container.name"
   ports {
     internal = 80
     external = 80
   }
+}
+
+#creating a docker network locally
+resource "docker_network" "my-network" {
+
 }
