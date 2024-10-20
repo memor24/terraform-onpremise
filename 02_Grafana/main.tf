82658b8429a566f566e9cb6830f29de7d2290c16
memor24linux <myemail@example.com>
2024-10-20 23:35:06 +0330

    updated grafana and prometheus

--- 02_Grafana/main.tf
+++ 02_Grafana/main.tf
@@ -1,32 +1,41 @@
 
 ## main:
 #creating a docker network
-resource "docker_network" "grafana_network" {
-    name= "grafana_network"
+resource "docker_network" "graf_network" {
+  name = "my_grafana_network"
 }
 
+# creating the image seperately to make sure it is also managed by terraform
+# this is how images are created in kreuzwerker/docker
+data "docker_registry_image" "grafana_image" {
+  name = "grafana/grafana:latest"
+}
+resource "docker_image" "grafana_image" {
+  name          = data.docker_registry_image.grafana_image.name
+  pull_triggers = [data.docker_registry_image.grafana_image.sha256_digest]
+} 
+
 #create a grafana container
-resource "docker_container" "grafana_container" {
-    name= "grafana_container"
-    image=  "grafana/grafana:latest"
-    keep_locally = "false"
-      
-    #define ports
-    ports {
-    internal=3000
-    external=3000
-    }
-    #mount volumes
-    volumes {
-        host_path="/var/lib/grafana"
-        container_path="/var/lib/grafana"
-    }
-    #connect the container to the network
-    networks_advanced{
-        name="docker_network.grafana_network.name"
-    }
-    #container auto restart policy
-    restart ="always"
+resource "docker_container" "graf_container" {
+  name         = "grafana_container"
+  image        = docker_image.grafana_image.name
+
+  #define ports
+  ports {
+    internal = 3000
+    external = 3000
+  }
+  #mount volumes
+  volumes {
+    host_path      = "/var/lib/grafana"
+    container_path = "/var/lib/grafana" 
+  }
+  #connect the container to the network
+  networks_advanced {
+    name = docker_network.graf_network.name
+  }
+  #container auto restart policy
+  restart = "always"
 }
 
 ##data sources:
@@ -36,5 +45,5 @@ data "grafana_cloud_ips" "test" {}
 
 ##outputs:
 output "grafana_url" {
-value = "http://localhost:3000"
+  value = "http://localhost:3000"
 }
