b6e8c13b218af4454ef8c4c6fab594b0f7d34cd6
memor24linux <myemail@example.com>
2024-10-21 18:47:26 +0330

    updates to 02,03, 04- prometheus/grafana/monitoring providers

--- 04_Monitoring/modules/prometheus/main.tf
+++ 04_Monitoring/modules/prometheus/main.tf
@@ -1,23 +1,46 @@
-terraform {
-  required_version = ">= 0.12"
-
-required_providers {
-     docker = {
-       source = "kreuzwerker/docker"
-     }
-}
+#creating docker network
+resource "docker_network" "prometh_network" {
+  name = "prometheus_network"
 }
 
-resource "docker_container" "prometheus" {
-image {"prometheus/prometheus:latest"}
-name {"prometheus"}
-restart {"always"}
-ports{
-internal=9090
-external=var.prometheus_port
+# creating the image seperately to make sure it is also managed by terraform
+# this is how images are created in kreuzwerker/docker
+data "docker_registry_image" "prometh_image" {
+  name = "prom/prometheus:latest"
+}
+resource "docker_image" "prometh_image" {
+  name          = data.docker_registry_image.prometh_image.name
+  pull_triggers = [data.docker_registry_image.prometh_image.sha256_digest]
 }
 
-network_advanced {
-name= var.network_name
+#creating prometheus container
+resource "docker_container" "prometh_container" {
+  name  = "prometheus_container"
+  image = docker_image.prometh_image.name
+
+  #port binding
+  ports {
+    internal = 9090
+    external = var.prom_port
+  }
+  #container volume for persistent prometheus data
+  volumes {
+    host_path      = "/etc/prometheus/prometheus.yml"
+    container_path = "/etc/prometheus"
+  }
+  #attaching container to network
+  networks_advanced {
+    name = docker_network.prometh_network.name
+  }
+  restart = "always"
 }
+########
+variable "prom_port" {
+  default = 9090
+  type    = number
+}
+##############
+
+output "prometheus_url" {
+  value = "http://localhost:9090"
 }
