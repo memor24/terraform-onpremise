21fdeb308da1bdd30583e8c5bd569d908d8a2cca
memor24linux <myemail@example.com>
2024-10-23 20:24:48 +0330

    updated dev tools

---
+++ 02_Dev_Tools/GitHub/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+    version= ">=0.13"
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.token # or `GITHUB_TOKEN`
+}
