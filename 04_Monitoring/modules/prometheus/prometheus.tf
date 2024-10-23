346095d6fee005bf025ce38a1f7db2286d988206
memor24linux <myemail@example.com>
2024-10-23 16:37:45 +0330

    updated motitoring

--- 04_Monitoring/modules/prometheus/prometheus.tf
+++ 04_Monitoring/modules/prometheus/prometheus.tf
@@ -1,9 +1,8 @@
-#creating docker network
-# resource "docker_network" "monitoring_network" {
-#   name = var.monitoring_network
-# }
+# Resources:
 
-# creating the image seperately to make sure it is also managed by terraform
+#creating docker network not needed here
+#prometheus module is connected to monitoring network via 04_Monitoring/main.tf with variables
+
 # this is how images are created in kreuzwerker/docker
 data "docker_registry_image" "prometh_image" {
   name = "prom/prometheus:latest"
@@ -12,6 +11,7 @@ resource "docker_image" "prometh_image" {
   name          = data.docker_registry_image.prometh_image.name
   pull_triggers = [data.docker_registry_image.prometh_image.sha256_digest]
 }
+# creating the image seperately to make sure it is also managed by terraform
 
 #creating prometheus container
 resource "docker_container" "prometh_container" {
@@ -36,7 +36,7 @@ resource "docker_container" "prometh_container" {
 }
 
 
-# Output
+# Outputs:
 output "prometheus_url" {
   value = "http://localhost:9090/metrics"
 }
