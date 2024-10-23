597eb9f44f0430f5dd346af388ee887b8ce8584c
memor24linux <myemail@example.com>
2024-10-24 02:22:57 +0330

    updated 03-grafana

--- 03_Grafana/grafana.tf
+++ 03_Grafana/grafana.tf
@@ -1,27 +1,36 @@
-###############################
+# Resources:
 
-provider "grafana" {
-  url  = "http://grafana.example.com/"
-  auth = var.grafana_auth
-}
-
-// Optional (On-premise, not supported in Grafana Cloud): Create an organization
+# Creating the organization (On-premise, not supported in Grafana Cloud)
 resource "grafana_organization" "my_org" {
   name = "my_org"
 }
 
-// Create resources (optional: within the organization)
-resource "grafana_folder" "my_folder" {
+# creating folder and its permission
+resource "grafana_folder" "org_folder" {
   org_id = grafana_organization.my_org.org_id
-  title  = "Test Folder"
+  title  = "test folder"
+}
+resource "grafana_folder_permission" "org_permission" {
+  folder_uid = grafana_folder.org_folder.uid
+  permissions {
+    role       = "Editor"
+    permission = "Edit"
+  }
 }
 
-resource "grafana_dashboard" "test_folder" {
+# creating dashboard
+resource "grafana_dashboard" "org_dashboard" {
   org_id = grafana_organization.my_org.org_id
-  folder = grafana_folder.my_folder.id
+  folder = grafana_folder.org_folder.id
   config_json = jsonencode({
     "title" : "My Dashboard Title",
     "uid" : "my-dashboard-uid"
-    // ... other dashboard properties
   })
 }
+
+# defining the data source
+resource "grafana_data_source" "prometheus" {
+  name       = "Prometheus"
+  type       = "prometheus"
+  url        = "http://prometheus:9090"
+  }
---
+++ 03_Grafana/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    grafana = {
+      source = "grafana/grafana"
+      version=">=1.0.0"
+    }
+  }
+}
+
+
+provider "grafana" {
+  url  = "http://localhost:3000" # or "http://grafana.example.com/"
+  auth = var.grafana_auth
+}
+
+variable "grafana_auth" {
+  default = "admin:admin"
+}
